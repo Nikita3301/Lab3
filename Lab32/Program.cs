769bfe6d2b8cd19bf64b9c34a17f4ad39be122c5
Nikita3301 <60880088+Nikita3301@users.noreply.github.com>
2020-05-22 19:16:30 +0300

    1

---
+++ Lab32/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System;
+
+namespace Lab32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter g = new StreamWriter("C:\\Users\\s\\RiderProjects\\Lab32\\g.txt");
+            StreamReader f = new StreamReader("C:\\Users\\s\\RiderProjects\\Lab32\\f.txt");
+            string str;
+            Console.WriteLine("Парнi числа з файлу:");
+            while ((str = f.ReadLine()) != null)
+            {
+                int number =Convert.ToInt32(str);
+                if (number % 2 == 0)
+                {
+                    g.WriteLine(number);
+                    Console.WriteLine(number);
+
+                }
+            }
+
+            g.Close();
+        }
+    }
+}
